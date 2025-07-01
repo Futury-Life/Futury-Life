313e8633ded2c48b9904fe5c7c799e075a5ddd22
Futury-lIFe <futurylife23@gmail.com>
2025-07-01 09:26:15 -0300

    Add files via upload

---
+++ CF/jogodavelha/src/main/java/br/com/calculadora/jogodavelha/Jogodavelha.java
@@ -0,0 +1,11 @@
+
+package br.com.calculadora.jogodavelha;
+public class Jogodavelha {
+
+    public static void main(String[] args) {
+
+        
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+}
